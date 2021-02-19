708a2788f2fcbd3b6070a2d675dcc193e4ad6e90
Premkumar Singaravelu <prem.kumar@hyperverge.co>
2021-02-19 14:27:38 +0530

    fix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,6 @@ const { details, edit } = require('../controller/userController');
 const validation = require("../middlewares/validation");
 var jwtToken = require('../middlewares/jwtToken');
 
-router.get('/details', validation, details);
+router.post('/details', details);
 router.post('/edit',  validation, edit);
 module.exports = router;
